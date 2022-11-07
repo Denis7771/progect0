2a88872f4b5f337f40061f1b82acbf45533a6f58
Denis7771 <bmb.black20@gmail.com>
2022-11-07 23:46:50 +0200

    8 commit

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def StructVision(GetStruct1):
         print(SubStruct)
 
 
-def StructDel(GetStruct):
+def StructDel(GetStruct):      #Функция удаление слова из словаря
     DelWord = input("Видалити слово: ")
     for SubStruct in GetStruct:
         if SubStruct['English'] == DelWord:
             GetStruct.pop(GetStruct.index(SubStruct))
 
-def StructAdd(GetStruct):
+
+def StructAdd(GetStruct):     #Создание Словаря
     items = ['параграф', 'English', 'Українська']
     SubStructDict = dict.fromkeys(items)
     EngWord = input("Введите слово:")
@@ -24,37 +25,37 @@ def StructAdd(GetStruct):
     GetStruct.append(SubStructDict)
 
 
-def SortStruct(e):
+def SortStruct(e):       #Функия сортировать словарь
     return e['параграф'][1]
 
 
 MainStruct = list()
 
-while True:
+while True:    #Меню
     print('''----------
     додати слово -- add
-    показати словник -- vision
+    показати словник -- vision    
     видалити слово -- del
     сортувати -- sort
     вихід -- exit
     ''')
     EventMenu = input("Виберіть потрібний пункт меню: ")
-    if EventMenu == "add":
+    if EventMenu == "add":  #Добавить слово в Словарь
         StructAdd(MainStruct)
         print()
         StructVision(MainStruct)
         print(f"\n Розмір словника: {sys.getsizeof(MainStruct)} байт")
-    elif EventMenu == "exit":
+    elif EventMenu == "exit":  #Выйти из Словаря
         MainStruct.clear()
-        del MainStruct
+        del MainStruct    #Удаляем для очистки памяти
         print("Бувай!!!")
         break
-    elif EventMenu == "vision":
+    elif EventMenu == "vision":   #Показать Словарь
         StructVision(MainStruct)
         print(f"\n Розмір словника: {sys.getsizeof(MainStruct)} байт")
-    elif EventMenu == "sort":
+    elif EventMenu == "sort":  #Сортировать Словарь
         MainStruct.sort(key=SortStruct)
-    elif EventMenu == "del":
+    elif EventMenu == "del":  #Удаление слова из Словаря
         StructDel(MainStruct)
         print()
         StructVision(MainStruct)
